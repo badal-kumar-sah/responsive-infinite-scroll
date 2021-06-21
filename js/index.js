faf9b8281d7faf7bc39f474c02c2566672ea6490
Badal Kumar Sah <badalsah211@gmail.com>
2021-06-21 14:27:53 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,72 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+let photosArray = [];
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+
+// Unsplash API
+let imageCount = 5;
+const apiKey = "iHmNNo1cQ8N8ayKnIJLMvZiB2ZGhDKPotW4zzSwYCo8";
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${imageCount}`;
+
+// Check if images are loaded
+function imageloaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    imagesLoaded = 0;
+    ready = true;
+    loader.hidden = true;
+    imageCount = 10;
+  }
+}
+
+// Helper function to set attribute
+function setAttributeHelper(elements, attributes) {
+  for (const key in attributes) {
+    elements.setAttribute(key, attributes[key]);
+  }
+}
+
+function displayPhotos() {
+  totalImages = photosArray.length;
+  // Running forEach loop for every photos fetched
+  photosArray.forEach((photo) => {
+    const anchorTag = document.createElement("a");
+    setAttributeHelper(anchorTag, { href: photo.links.html, target: "_blank" });
+    const img = document.createElement("img");
+    setAttributeHelper(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+
+    // Appending the tags within each-other
+    imageloaded();
+    anchorTag.appendChild(img);
+    imageContainer.appendChild(anchorTag);
+  });
+}
+
+// Get photos from API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {}
+}
+
+// If scroller at the bottom of page, get more photos
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
+
+// On Load
+getPhotos();
